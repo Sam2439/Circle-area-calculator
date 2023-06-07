b5088534c73103c7943323e5cb960b88d425727b
Sam2439 <126176328+Sam2439@users.noreply.github.com>
2023-06-07 20:27:57 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,12 @@
+#Program to find area of circle
+#For better result we use Excact value of PI from "math" which is a python package
+import math
+from math import pi
+#taking value of radius from user in "r" variable
+r=int(input("Enter your Radius :"))
+area=pi*r**2
+print('The area of circle is =',area)
+#Made by :
+#Sameer
+#X C
+#K.V. J.N.U. Campus
